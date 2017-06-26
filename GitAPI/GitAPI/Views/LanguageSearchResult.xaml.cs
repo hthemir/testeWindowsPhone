9f63e9a38a00238f7017d0693919d1b0add10dbc
Hugo Themir <hthemir@hotmail.com>
2017-06-26 14:40:25 -0300

    git api

--- GitAPI/GitAPI/Views/LanguageSearchResult.xaml.cs
+++ GitAPI/GitAPI/Views/LanguageSearchResult.xaml.cs
@@ -1,46 +1,27 @@
 ﻿using Model;
-using Newtonsoft.Json;
 using Service;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
-
 namespace GitAPI.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class LanguageSearchResult : Page
     {
+        int page { get; set; }
+        string linguagem { get; set; }
+
         public LanguageSearchResult()
         {
             this.InitializeComponent();
         }
-
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
+        
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            string url = "https://api.github.com/search/repositories?q=language:" + e.Parameter.ToString() +"&sort=stars&page=1";
+            page = 1;
+            linguagem = e.Parameter.ToString();
+            string url = "https://api.github.com/search/repositories?q=language:" + linguagem +"&sort=stars&page=1";
             fillScreen(url);
         }
 
@@ -53,27 +34,19 @@ namespace GitAPI.Views
             {
                 resultList.Items.Add(items[i]);
             }
-
-            /*Repository batata1 = new Repository();
-            batata1.full_name = "batata1";
-            batata1.description = "a primeira batata";
-            resultList.Items.Add(batata1);
-            Repository batata2 = new Repository();
-            batata2.full_name = "batata2";
-            batata2.description = "a segunda batata";
-            resultList.Items.Add(batata2);*/
-        }
-        
-
-        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            //Frame.Navigate(typeof(RepositorySearchResult));
         }
 
         private void resultList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var mySelectedItem = resultList.SelectedItem as Repository;
             Frame.Navigate(typeof(RepositorySearchResult), mySelectedItem);
+        }
+
+        private void loadMoreResults_Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            page += 1;
+            string url = "https://api.github.com/search/repositories?q=language:" + linguagem + "&sort=stars&page=" + page;
+            fillScreen(url);
         }
     }
 }
--- GitAPI/GitAPI/Views/RepositorySearchResult.xaml.cs
+++ GitAPI/GitAPI/Views/RepositorySearchResult.xaml.cs
@@ -1,27 +1,12 @@
 ﻿using Model;
 using Service;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace GitAPI.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class RepositorySearchResult : Page
     {
         public RepositorySearchResult()
@@ -29,11 +14,6 @@ namespace GitAPI.Views
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             Repository rep = e.Parameter as Repository;
